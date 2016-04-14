d89f60645b974401cd1c8df88789964abf3e83b9
aTo <aTo@ato_mac.home>
2016-04-14 12:50:07 +0200

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by aTo on 14/04/2016.
+//  Copyright © 2016 aTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by aTo on 14/04/2016.
+//  Copyright © 2016 aTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
